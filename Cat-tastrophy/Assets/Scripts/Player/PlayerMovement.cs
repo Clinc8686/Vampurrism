ac8a319899306b407f1e9acda0f902a47ca43c6c
Clinc8686 <mario1.lanpert@gmail.com>
2022-04-05 18:01:44 +0200

    Bugfix playerMovement

--- Cat-tastrophy/Assets/Scripts/Player/PlayerMovement.cs
+++ Cat-tastrophy/Assets/Scripts/Player/PlayerMovement.cs
@@ -19,21 +19,16 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movementDirection;
     private Vector3 mousePosition;
     private Vector2 movementCoordinates;
-    private int munition = 10;
+    private int munition = 100000000;
 
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        Aim();
-    }
-
     void FixedUpdate()
     {
-        
+        Aim();
         playerRB.velocity += movementCoordinates * Time.deltaTime;
     }
 
@@ -75,21 +70,24 @@ public class PlayerMovement : MonoBehaviour
 
         GameObject arrow = Instantiate(arrowPrefab, spawnPositionVaccine.position, Quaternion.identity);
         arrow.transform.Rotate(0, 0,270.0f);
-        arrow.GetComponent<Rigidbody2D>().velocity = worldPoint * ARROW_BASE_SPEED;
-        arrow.transform.Rotate(0,0,Mathf.Atan2(worldPoint.y, worldPoint.x) * Mathf.Rad2Deg);
+        
+        Vector3 crossHairPosition = crossHair.transform.localPosition;
+        arrow.GetComponent<Rigidbody2D>().velocity = crossHairPosition * ARROW_BASE_SPEED;
+        arrow.transform.Rotate(0,0,Mathf.Atan2(crossHairPosition.y, crossHairPosition.x) * Mathf.Rad2Deg);
         Destroy(arrow, 3.0f);
     }
     
     void Aim()
     {
         mousePosition = Mouse.current.position.ReadValue();
+        Debug.Log("mouse x:" + mousePosition.x + "y:" + mousePosition.y);
         mousePosition.z = Camera.main.farClipPlane;
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(mousePosition);
-
-        Vector3 movementDir = new Vector2(worldPoint.x, worldPoint.y);
-        movementDir.Normalize();
-        
-        crossHair.transform.localPosition = movementDir * AIM_MAX_DISTANCE;
-
+        Debug.Log("world x:" + worldPoint.x + "y:" + worldPoint.y);
+        Vector2 aimDirection = new Vector2(worldPoint.x, worldPoint.y);
+        aimDirection = aimDirection + playerRB.position;
+        aimDirection.Normalize();
+        Debug.Log("aim x:" + aimDirection.x + "y:" + aimDirection.y);
+        crossHair.transform.localPosition = (aimDirection) * AIM_MAX_DISTANCE;
     }
 }
