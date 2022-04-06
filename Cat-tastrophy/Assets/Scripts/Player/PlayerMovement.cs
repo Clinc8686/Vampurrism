b800e8630fbce70fa7cd4072652d9c5309b7f66d
Clinc8686 <mario1.lanpert@gmail.com>
2022-04-07 01:20:55 +0200

    added animation to player (move and idle), flipping player on sideways, added collision to player, shooting from correct position

--- Cat-tastrophy/Assets/Scripts/Player/PlayerMovement.cs
+++ Cat-tastrophy/Assets/Scripts/Player/PlayerMovement.cs
@@ -10,17 +10,17 @@ public class PlayerMovement : MonoBehaviour
 { 
     [SerializeField] private Rigidbody2D playerRB;
     [SerializeField] private GameObject arrowPrefab;
-    [SerializeField] private GameObject waterDropPrefab;
     [SerializeField] private Transform spawnPositionWater;
     [SerializeField] private Transform spawnPositionVaccine;
     [SerializeField] private RectTransform crossHairUI;
     [SerializeField] private GameObject waterParticlesPrefab;
-    private float AIM_MAX_DISTANCE = 300.0f;
-    private float PLAYER_SPEED = 7.0f;
-    private float ARROW_BASE_SPEED = 8.0f;
-    private float WATER_BASE_SPEED = 3.0f;
+    [SerializeField] private GameObject body;
+    private Animator animator;
+    private float AIM_MAX_DISTANCE = 2.0f;
+    private float PLAYER_SPEED = 5.0f;
+    private float ARROW_BASE_SPEED = 15.0f;
+    private float WATER_BASE_SPEED = 12.0f;
     private Vector2 movementDirection;
-    private Vector3 mousePosition;
     private Vector2 movementCoordinates;
     private int MAX_VACCINE_MUNITION = 10;
     private int vaccineMunition;
@@ -28,10 +28,12 @@ public class PlayerMovement : MonoBehaviour
     private int waterMunition;
     private Vector2 screenCoords;
     private bool refill = false;
+    private bool rotated = false;
+    private bool moving = false;
 
     void Start()
     {
-        playerRB = GetComponent<Rigidbody2D>();
+        animator = body.GetComponent<Animator>();
         vaccineMunition = MAX_VACCINE_MUNITION;
         waterMunition = MAX_WATER_MUNITION;
     }
@@ -44,10 +46,44 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         playerRB.velocity += movementCoordinates * Time.deltaTime;
+        Vector3 playerLocalScale = transform.localScale;
+        if (rotated)
+        {
+            transform.localScale = new Vector3(-1, playerLocalScale.y, playerLocalScale.z);
+        } else
+        {
+            transform.localScale = new Vector3(1, playerLocalScale.y, playerLocalScale.z);
+        }
+
+        if (moving)
+        {
+            animator.SetBool("Walking", true);
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
     }
 
     private void OnMove(InputValue value)
-    {
+    { 
+        Vector2 input = value.Get<Vector2>();
+        if (input.x > 0)
+        {
+            rotated = true;
+        } else if (input.x < 0)
+        {
+            rotated = false;
+        }
+
+        if (Vector2.Distance(input, new Vector2(0,0)) > 0)
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false; 
+        }
         movementDirection = value.Get<Vector2>();
         playerRB.velocity = new Vector2(movementDirection.normalized.x, movementDirection.normalized.y) * PLAYER_SPEED;
     }
@@ -112,15 +148,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void ShootVaccine()
     {
-        mousePosition = Mouse.current.position.ReadValue();
-        mousePosition.z = Camera.main.farClipPlane;
-
         GameObject arrow = Instantiate(arrowPrefab, spawnPositionVaccine.position, Quaternion.identity);
         arrow.transform.Rotate(0, 0,90);
         
-        Rigidbody2D player = arrow.GetComponent<Rigidbody2D>();
-        player.velocity = screenCoords * ARROW_BASE_SPEED;
-        arrow.transform.up = new Vector3(player.velocity.x, player.velocity.y, 0.0f);
+        //---
+        Rigidbody2D arrowRigidbody2D = arrow.GetComponent<Rigidbody2D>();
+        arrowRigidbody2D.velocity = screenCoords * ARROW_BASE_SPEED;
+        arrow.transform.up = new Vector3(arrowRigidbody2D.velocity.x, arrowRigidbody2D.velocity.y, 0.0f);
+        //Rigidbody2D player = arrow.GetComponent<Rigidbody2D>();
+        //player.velocity = screenCoords * ARROW_BASE_SPEED;
+        //arrow.transform.up = new Vector3(player.velocity.x, player.velocity.y, 0.0f);
         Destroy(arrow, 3.0f);
     }
     
@@ -129,11 +166,12 @@ public class PlayerMovement : MonoBehaviour
         Vector2 pixelCoords = Mouse.current.position.ReadValue();
         float x = pixelCoords.x / Screen.width;
         float y = pixelCoords.y / Screen.height;
-        screenCoords = new Vector2(x, y);
-        screenCoords.x = (screenCoords.x - 0.5f) * 2.0f;
-        screenCoords.y = (screenCoords.y - 0.5f) * 2.0f;
+        Vector2 scCoords = new Vector2(x, y);
+        screenCoords.x = (scCoords.x - 0.5f) * 2.0f;
+        screenCoords.y = (scCoords.y - 0.5f) * 2.0f;
+        Debug.Log("vorher x:" + screenCoords.x + " y:" + screenCoords.y);
         screenCoords.Normalize();
-        
+        Debug.Log("nachher x:" + screenCoords.x + " y:" + screenCoords.y);
         crossHairUI.localPosition = screenCoords * AIM_MAX_DISTANCE;
     }
 }
