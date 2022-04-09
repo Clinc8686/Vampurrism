00afa81a673a2acaa2f757dbf575022693c7c5a0
Jule Bauer <miletti67@gmail.com>
2022-04-09 12:58:39 +0200

    Food Interaction made clearer

--- Cat-tastrophy/Assets/Scripts/Food.cs
+++ Cat-tastrophy/Assets/Scripts/Food.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Food : MonoBehaviour
 {
     public int _regeneratedHealth = 1;
+    public GameObject interactIndicator;
 
     public void OnPickUpFood()
     {
@@ -12,5 +13,21 @@ public class Food : MonoBehaviour
     public int GetRegenHealth()
     {
         return _regeneratedHealth;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            interactIndicator.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            interactIndicator.SetActive(false);
+        }
     }
 }
