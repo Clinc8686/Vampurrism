8e3624a2370aa31f3979a85761dfbc8d00d8b2b7
Clinc8686 <mario1.lanpert@gmail.com>
2022-04-09 16:35:11 +0200

    removed old life pictures, added new life pictures, bugfix in life

--- Cat-tastrophy/Assets/Scripts/Player/PlayerMovement.cs
+++ Cat-tastrophy/Assets/Scripts/Player/PlayerMovement.cs
@@ -110,6 +110,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D col)
     {
+        oldTime = DateTime.Now;
         if (col.CompareTag("WaterWell"))
         {
             refill = true;
